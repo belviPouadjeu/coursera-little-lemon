f8fca9d890f7c76672cc2196156ed06f30f72a9d
belviPouadjeu <pbelvinard21@gmail.com>
2025-05-31 19:38:26 +0200

    Feat : setting up project

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="/home">Homepage</a></li>
+                    <li><a href="/about">About Us</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                </ul>
+                </nav>
+        </div>
+    )
+}
+
+export default Nav
